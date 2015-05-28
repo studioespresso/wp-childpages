49918798bd340f7cce77bc0ac2fdb11e819ea808
Jan Henckens <jan@onedge.be>
2015-05-28 20:26:22 +0200

    Add widget title field

--- includes/class-wp-sub-pages-widget.php
+++ includes/class-wp-sub-pages-widget.php
@@ -14,10 +14,61 @@
                 array( 'description' => __( 'A Foo Widget', 'text_domain' ), ) // Args
             );
         }
-        
+
+
+        /**
+         * Display the widget
+         * @param $args
+         * @param $instance
+         */
+        function widget($args, $instance)
+        {
+            extract($args);
+
+            // The widget title
+            $title = apply_filters('widget_title', $instance['title']);
+            $before_title = "<div class='widget-title'>";
+            $after_title = "</div>";
+            ?>
+            <aside id="wp-sub-pages" class="widget wp-sub-pages">
+                <?php if ( $title ) echo $before_title . $title . $after_title; ?>
+            </aside>
+            <?php
+        }
+
+
+        /**
+         * Update the widget
+         * @param $new_instance
+         * @param $old_instance
+         * @return mixed
+         */
+        function update( $new_instance, $old_instance ) {
+            $instance = $old_instance;
+            $instance['title'] = strip_tags( $new_instance['title'] );
+            return $instance;
+        }
+
+        /**
+         * Render the widget form in the Dashboard
+         * @param $instance
+         */
+        function form($instance) {
+
+            //Set up some default widget settings.
+            $defaults = array( 'title' => __('Our sponsors', 'wp-sponsors'), 'check_images' => 'on' , 'category' => 'All'); ?>
+
+            <p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'wp-sponsors'); ?></label>
+                <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+            </p>
+        <?php }
+
     }
 
-    // register Foo_Widget widget
+    /**
+     * Register the widget
+     */
     function register_subpages_widget() {
         register_widget( 'Subpages_Widget' );
     }
